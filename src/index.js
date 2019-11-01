495a5ee251795b609241d3e9ff0750cd0fd66a6b
Oguz Kumcular <oguzkumcular@gmail.com>
2019-11-01 19:52:33 +0300

    Removing files and add bootstrap

--- src/index.js
+++ src/index.js
@@ -4,21 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function tick(params) {
-    const element = (
-        <div>
-            <h1>
-                Hello, World
-            </h1>
-            
-                it is {new Date().toLocaleTimeString()}.
-            
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-}
-
-setInterval(tick,1000);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
