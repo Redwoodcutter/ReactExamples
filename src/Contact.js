72402d2736a070289146965f8ac643ba3a046e73
Oguz Kumcular <oguzkumcular@gmail.com>
2019-11-01 21:44:46 +0300

    Page components created

---
+++ src/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const Contact = () =>{
+    return(
+        <div>
+            <h1>Hello, Stranger. This is Contact Page.</h1>
+            <p>You have to make almighty decisions when you're the creator. We touch the canvas, the canvas takes what it wants.
+                 Go out on a limb - that's where the fruit is. The least little bit can do so much. This is your world. I guess I'm a little weird. 
+                I like to talk to trees and animals. That's okay though; I have more fun than most people.
+            </p>
+            <p>
+            In painting, you have unlimited power. You have the ability to move mountains. Learn when to stop. Use what you see, don't plan it.
+            </p>
+            <p>
+            That's what makes life fun. That you can make these decisions. That you can create the world that you want. 
+            Only think about one thing at a time. Don't get greedy. These trees are so much fun. I get started on them and I have a hard time stopping.
+             Just let these leaves jump off the brush Put your feelings into it, your heart, it's your world. All you have to do is let your imagination 
+             go wild.
+            </p>
+
+        </div>
+    )
+}
---
+++ src/NoMatch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const NoMatch = () =>{
+    return(
+        <div>
+            <h1>You Shall Not Pass, Someting wrong mate. We didnt find your request...</h1>
+        
+        </div>
+    )
+}
